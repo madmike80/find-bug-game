ea9f017c96bb57a94252858a401d98369a67e67a
Mihail <mihail.treschalin@ya.ru>
2020-03-14 23:26:24 +0300

    реализованÐ выбор сложности игрыÐ

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
+let menu = document.querySelectorAll(".list-item");
+
+for (let item of menu) {
+  item.addEventListener("click", () => {
+    for (let a of menu) {
+      a.classList.remove("list-item_checked");
+    }
+    item.classList.add("list-item_checked");
+  });
+}
+
+
 let isFlip = (className) => {
   const elem = document.querySelector(`.${className}`);
-  elem.addEventListener('click', () => {
-    elem.classList.toggle('flip');
+  elem.addEventListener("click", () => {
+    elem.classList.toggle("flip");
   })
-}
+};
+
 
 isFlip('card');
