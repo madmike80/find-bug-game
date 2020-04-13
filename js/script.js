09585b7c1e48c19cf19c128a8bf700e32a20907f
Mihail <mihail.treschalin@ya.ru>
2020-04-14 00:44:32 +0300

    Added a function to delete the event handler after the card is flipped.

--- js/script.js
+++ js/script.js
@@ -17,6 +17,7 @@ let startGame = () => {
   const playingMenu = document.querySelector(".menu");
   const playingGame = document.querySelector(".game");
   const card = document.querySelector(".card");
+
   switch (`${level.innerHTML}`) {
     case "Простой" :
       count = 3;
@@ -45,17 +46,38 @@ let startGame = () => {
     }
   };
 
+  let changeCardBug = () => {
+    const gameCards = document.querySelectorAll(".card");
+
+    let randomCard = Math.floor(Math.random() * count);
+
+    let cardFace = gameCards[randomCard].querySelector(".card__side_face");
+    cardFace.classList.toggle("hide");
+
+    let cardBug = gameCards[randomCard].querySelector(".card__side_bug");
+    cardBug.classList.toggle("hide");
+  };
+
   let isFlip = (className) => {
+
+    function clickEvent() {
+      this.classList.add("flip");
+      this.classList.remove("card_hover");
+
+      for (let el of elem) {
+        el.removeEventListener("click", clickEvent);
+      }
+    }
+
     const elem = document.querySelectorAll(`.${className}`);
+
     for (let item of elem) {
-      item.addEventListener("click", () => {
-        item.classList.add("flip");
-        item.classList.remove("card_hover");
-      })
+      item.addEventListener("click", clickEvent);
     }
   };
 
   changePlayingField();
+  changeCardBug();
   isFlip('card');
 };
 
